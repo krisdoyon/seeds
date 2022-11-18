a93da5fd3352633c09c04044d1e3d31212ed9bc5
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-17 22:09:54 -0500

    Add main nav links, add initial navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const navLinks = [
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "About",
+    to: "/about",
+  },
+  {
+    text: "Shop",
+    to: "/shop",
+  },
+];
+
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <NavLink
-        to="/"
-        className={`nav__link ${({ isActive }) =>
-          isActive ? "nav__link--active" : ""}`}
-      >
-        Home
-      </NavLink>
-    </nav>
+    <header className="header">
+      <nav className="nav">
+        <ul className="nav__list">
+          {navLinks.map((link, i) => {
+            return (
+              <li key={i}>
+                <NavLink
+                  to={link.to}
+                  className={`nav__link ${({ isActive }) =>
+                    isActive ? "nav__link--active" : ""}`}
+                >
+                  {link.text}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
