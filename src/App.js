f12bc2ec273913f0b945ea2f7b8be21964ea2b5b
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-17 22:07:26 -0500

    Setup initial routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 // PAGE COMPONENTS
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import SingleProduct from "./pages/SingleProduct";
 import PageNotFound from "./pages/PageNotFound";
 import Layout from "./pages/Layout";
 // SASS
@@ -13,6 +16,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="shop/:id" element={<SingleProduct />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
