21913fe2015d88c0730082e2941db18d6d8b2cf7
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-18 21:05:28 -0500

    Add data and back button to single product component

--- src/pages/SingleProduct/SingleProduct.js
+++ src/pages/SingleProduct/SingleProduct.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import products from "../../data/seeds.json";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../context/context";
 
 const Product = () => {
   const { id } = useParams();
-  const product = products.find((product) => product.id === id);
-  return <div>{product.title}</div>;
+  const { getProduct } = useGlobalContext();
+  const product = getProduct(id);
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <button onClick={() => navigate(-1)}>Back</button>
+      <h1>{product.title}</h1>
+      <img src={`/img/${product.id}.webp`} alt="" />
+    </div>
+  );
 };
 
 export default Product;
