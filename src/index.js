c38682b52f4bd190cbdbcaddaafb7c5df05201a6
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-18 21:07:11 -0500

    Include global context provider

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import ScrollToTop from "./helpers/ScrollToTop";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AppProvider } from "./context/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <ScrollToTop />
-        <App />
-      </Router>
+      <AppProvider>
+        <Router>
+          <ScrollToTop />
+          <App />
+        </Router>
+      </AppProvider>
     </Provider>
   </React.StrictMode>
 );
