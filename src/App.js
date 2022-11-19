b4ecc6868e02914c526bd219ae602c5f95af7eac
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-18 21:06:58 -0500

    Update react router routes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Routes, Route } from "react-router-dom";
 // PAGE COMPONENTS
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Shop from "./pages/Shop";
 import SingleProduct from "./pages/SingleProduct";
+import Products from "./pages/Products";
 import PageNotFound from "./pages/PageNotFound";
 import Layout from "./pages/Layout";
 // SASS
@@ -17,8 +17,12 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="shop/:id" element={<SingleProduct />} />
+        <Route path="shop">
+          <Route index element={<Products />} />
+          <Route path=":category" element={<Products />} />
+        </Route>
+        <Route path="/shop/:category/:id" element={<SingleProduct />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
