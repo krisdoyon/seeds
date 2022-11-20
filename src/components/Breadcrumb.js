7ce6b65990a1140c4c90963456900f4d1712a580
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-19 22:22:13 -0500

    Add font weight to breadcrumb title, hover effect to links

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -5,21 +5,29 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   text-transform: capitalize;
   margin-bottom: 2rem;
-
+  font-size: 1.4rem;
   a {
     text-decoration: none;
     color: inherit;
+    font-family: inherit;
+
+    &:hover {
+      font-weight: 600;
+    }
+  }
+
+  .breadcrumb__title {
+    font-weight: 600;
   }
 `;
 
 const Breadcrumb = ({ title, category, product }) => {
   return (
     <Wrapper>
-      <span>
-        <Link to="/">Home</Link> /
-        {(category || product) && <Link to="/shop"> Shop /</Link>}
-        {product && <Link to={`/shop/${category}`}> {category} /</Link>} {title}
-      </span>
+      <Link to="/">Home</Link> /
+      {(category || product) && <Link to="/shop"> Shop /</Link>}
+      {product && <Link to={`/shop/${category}`}> {category} /</Link>}
+      <span className="breadcrumb__title"> {title}</span>
     </Wrapper>
   );
 };
