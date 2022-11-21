c87cf3b635aa06b168120b051a208a429b15db7e
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-20 19:28:13 -0500

    Remove app provider from index

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ import ScrollToTop from "./helpers/ScrollToTop";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AppProvider } from "./context/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppProvider>
-        <Router>
-          <ScrollToTop />
-          <App />
-        </Router>
-      </AppProvider>
+      <Router>
+        <ScrollToTop />
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
