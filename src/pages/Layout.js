6b112e4b16886dbca68b20116c55483bde552ff5
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-20 19:31:08 -0500

    Add banner to common layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <>
+      <Banner />
       <Navbar />
       <Outlet />
       <Footer />
