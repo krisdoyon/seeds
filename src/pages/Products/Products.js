f4d1961f5829cf3cc11b9e377248e04045c225ee
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-20 19:30:47 -0500

    Fix products component grid sizing

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,42 +1,67 @@
 import { useParams } from "react-router-dom";
 import ProductNav from "./ProductNav";
-import { useGlobalContext } from "../../context/context";
 import ProductCard from "./ProductCard";
 import Breadcrumb from "../../components/Breadcrumb";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadProducts } from "../../features/productsSlice";
 
 const Wrapper = styled.div`
+  margin-top: 4rem;
+
   .heading {
     text-transform: capitalize;
-    padding: 4rem 0;
+    margin-bottom: 0.6rem;
     font-size: 3.2rem;
   }
 
   .content {
     display: grid;
-    grid-template-columns: 25rem 1fr;
+    grid-template-columns: 28rem 1fr;
     gap: 1rem;
+    margin-top: 3rem;
   }
 
   .products {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: max-content;
     gap: 1rem;
+
+    &__header {
+      grid-column: 1/-1;
+      margin-bottom: 1.8rem;
+      gap: 1.4rem;
+    }
   }
 `;
 
 const Products = () => {
-  const { category } = useParams();
-  const { getProducts } = useGlobalContext();
-  const products = getProducts(category || "all");
+  let { category } = useParams();
+  // if (!category) category = "all";
+  const dispatch = useDispatch();
+  const { filters } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(loadProducts(category));
+  }, [category, filters]);
+
+  const { products } = useSelector((state) => state.products);
 
   return (
     <Wrapper className="container">
-      <h2 className="heading">{category || "all"}</h2>
       <Breadcrumb title={category || "shop"} category={category} />
       <div className="content">
         <ProductNav />
         <div className="products">
+          <header className="products__header">
+            <h2 className="heading">{category || "all"}</h2>
+            <span>{`${products.length} matching ${
+              products.length === 1 ? "product" : "products"
+            }`}</span>
+          </header>
+
           {products.map((product) => (
             <ProductCard key={product.id} {...product} />
           ))}
