b5b0945ac34f16c502c6961128273cf06ba01c9f
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-21 22:29:01 -0500

    Add popular section data

---
+++ src/data/popular.js
@@ -0,0 +1,22 @@
+export const popularProducts = [
+  {
+    title: "tomatoes",
+    link: "/shop/tomatoes",
+    img: "img/popular/popular1.webp",
+  },
+  {
+    title: "herbs",
+    link: "/shop/herbs",
+    img: "img/popular/popular2.webp",
+  },
+  {
+    title: "peppers",
+    link: "/shop/peppers",
+    img: "img/popular/popular3.webp",
+  },
+  {
+    title: "flowers",
+    link: "/shop/flowers",
+    img: "img/popular/popular4.webp",
+  },
+];
