5caa7f7c4a94812d73ee8f3ee89a1716597d92fb
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-21 22:29:11 -0500

    Update react router routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { useEffect } from "react";
 // REACT ROUTER
 import { Routes, Route } from "react-router-dom";
-// PAGE COMPONENTS
+// LAYOUT
+import Layout from "./pages/Layout";
+
+// MAIN PAGE COMPONENTS
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleProduct from "./pages/SingleProduct";
-import Products from "./pages/Products";
+import Shop from "./pages/Shop";
 import PageNotFound from "./pages/PageNotFound";
-import Layout from "./pages/Layout";
+// FOOTER PAGE COMPONENTS
+import FAQ from "./pages/FAQ";
+import Shipping from "./pages/Shipping";
+import Terms from "./pages/Terms";
+
 // SASS
 import "./sass/main.scss";
 import Cart from "./pages/Cart";
 
 import { calculateTotals } from "./features/cartSlice";
 import { useSelector, useDispatch } from "react-redux";
+import Returns from "./pages/Returns";
+import Wishlist from "./pages/Wishlist";
+
+import { updateWishlistAmount } from "./features/wishlistSlice";
 
 function App() {
   const dispatch = useDispatch();
   const { cartItems, promo } = useSelector((state) => state.cart);
+  const { wishlistItems } = useSelector((state) => state.wishlist);
 
   useEffect(() => {
     dispatch(calculateTotals());
@@ -27,17 +39,28 @@ function App() {
     localStorage.setItem("cart", JSON.stringify(cartItems));
   }, [cartItems]);
 
+  useEffect(() => {
+    dispatch(updateWishlistAmount());
+    localStorage.setItem("wishlist", JSON.stringify(wishlistItems));
+  }, [wishlistItems]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="shop">
-          <Route index element={<Products />} />
-          <Route path=":category" element={<Products />} />
+          <Route index element={<Shop />} />
+          <Route path=":category" element={<Shop />} />
         </Route>
         <Route path="/shop/:category/:id" element={<SingleProduct />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="wishlist" element={<Wishlist />} />
+        <Route path="faq" element={<FAQ />} />
+        <Route path="terms" element={<Terms />} />
+        <Route path="shipping" element={<Shipping />} />
+        <Route path="return" element={<Returns />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
