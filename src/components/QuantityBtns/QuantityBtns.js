13202ec0961797f707adf725c8d6ed804eba3aab
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-22 23:03:47 -0500

    Add small version of quantity buttons for cart preview

--- src/components/QuantityBtns/QuantityBtns.js
+++ src/components/QuantityBtns/QuantityBtns.js
@@ -8,15 +8,18 @@ const QuantityBtns = ({
   handleDecrease,
   handleIncrease,
   className,
+  small,
 }) => {
   return (
-    <div className={`${styles.wrapper} ${className}`}>
+    <div
+      className={`${styles.wrapper} ${className} ${small ? styles.small : ""}`}
+    >
       <Button className={styles.decrease} onClick={handleDecrease}>
-        <FaMinus />
+        <FaMinus className={styles.icon} />
       </Button>
       <div className={styles.quantity}>{quantity}</div>
       <Button className={styles.increase} onClick={handleIncrease}>
-        <FaPlus />
+        <FaPlus className={styles.icon} />
       </Button>
     </div>
   );
