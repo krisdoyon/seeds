8126d5bd785377cb58ae13b80a401d510fd55d8c
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-22 23:06:53 -0500

    Add modal reducer to store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
 import productsReducer from "./features/productsSlice";
 import wishlistReducer from "./features/wishlistSlice";
+import modalReducer from "./features/modalSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productsReducer,
     wishlist: wishlistReducer,
+    modal: modalReducer,
   },
 });
