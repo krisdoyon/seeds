f1ad25e425a2fcb1b1c9dd0c98a82f31abbee0ef
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-23 17:41:29 -0500

    Update slider data and images

--- src/pages/Home/Slider/slidesData.js
+++ src/pages/Home/Slider/slidesData.js
@@ -1,30 +1,32 @@
 export const slides = [
   {
+    title: "Save up to 40%",
+    text: "On select sale items!",
+    to: "/shop",
+    image: "/img/slider/slider_4.webp",
+    position: "center",
+    filter: "onSale",
+  },
+  {
     title: "Tomatoes",
-    text: "We have a great selection of tomatoes!",
+    text: "Shop our huge selection of tomatoes!",
     to: "/shop/tomatoes",
     image: "/img/slider/slider_1.webp",
     position: "center",
   },
   {
+    title: "New for 2022",
+    text: "Check out our newly added varieties!",
+    to: "/shop",
+    image: "/img/slider/slider_3.webp",
+    position: "top",
+    filter: "new",
+  },
+  {
     title: "Peppers",
-    text: "Huge pepper selection!",
+    text: "Check out our wide variety of sweet/hot peppers!",
     to: "/shop/peppers",
     image: "/img/slider/slider_2.webp",
     position: "top",
-  },
-  {
-    title: "Beets",
-    text: "Lots of beet varieties!",
-    to: "/shop/beets",
-    image: "/img/slider/slider_3.webp",
-    position: "center",
-  },
-  {
-    title: "Carrots",
-    text: "So many carrots!",
-    to: "/shop/carrots",
-    image: "/img/slider/slider_4.webp",
-    position: "center",
   },
 ];
