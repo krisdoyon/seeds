7e0be56cc04c7c5402138654ee1f46227e77b095
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-26 22:07:43 -0500

    Update routes to include orders and single orders pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import About from "./pages/About";
 import Cart from "./pages/Cart";
 import SingleProduct from "./pages/SingleProduct";
 import Shop from "./pages/Shop";
+import Orders from "./pages/Orders";
+import SingleOrder from "./pages/SingleOrder/SingleOrder";
 import PageNotFound from "./pages/PageNotFound";
 // FOOTER PAGE COMPONENTS
 import { Shipping, Terms, Returns } from "./pages/Policies";
@@ -24,14 +26,18 @@ import Wishlist from "./pages/Wishlist";
 
 import { updateWishlistAmount } from "./features/wishlistSlice";
 import Checkout from "./pages/Checkout/Checkout";
+import Overlay from "./components/Overlay";
+import { closeModal } from "./features/modalSlice";
 
 function App() {
   const dispatch = useDispatch();
   const { cartItems, promo } = useSelector((state) => state.cart);
   const { wishlistItems } = useSelector((state) => state.wishlist);
+  const { orders } = useSelector((state) => state.orders);
   const { isConfirmModalOpen, isPromoModalOpen } = useSelector(
     (state) => state.modal
   );
+  const { products } = useSelector((state) => state.products);
 
   useEffect(() => {
     dispatch(calculateTotals());
@@ -46,9 +52,19 @@ function App() {
     localStorage.setItem("wishlist", JSON.stringify(wishlistItems));
   }, [wishlistItems, dispatch]);
 
+  useEffect(() => {
+    localStorage.setItem("orders", JSON.stringify(orders));
+    // localStorage.setItem("products", JSON.stringify(products));
+  }, [orders, products]);
+
   return (
     <>
-      {(isConfirmModalOpen || isPromoModalOpen) && <Modal />}
+      {(isConfirmModalOpen || isPromoModalOpen) && (
+        <>
+          <Overlay onClick={() => dispatch(closeModal())} />
+          <Modal />
+        </>
+      )}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -60,6 +76,8 @@ function App() {
           <Route path="/shop/:category/:id" element={<SingleProduct />} />
           <Route path="cart" element={<Cart />} />
           <Route path="checkout" element={<Checkout />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="/orders/:id" element={<SingleOrder />} />
           <Route path="wishlist" element={<Wishlist />} />
           <Route path="faq" element={<FAQ />} />
           <Route path="terms" element={<Terms />} />
