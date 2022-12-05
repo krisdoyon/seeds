811cfe4719c076859d26675866040c796463f982
Kris Doyon <kristopher.doyon@gmail.com>
2022-12-04 22:21:29 -0500

    Update login component and form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,35 @@
 import styles from "./Login.module.scss";
+import { useEffect, useState } from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 import LoginForm from "./LoginForm/LoginForm";
+import { useSelector, useDispatch } from "react-redux";
+import Button from "../../components/Button";
+import { sendTestRequest } from "../../features/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(true);
+  const { userId, token, isLoggedIn } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/account");
+    }
+  }, [isLoggedIn]);
+
+  const toggleAuthState = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
   return (
     <section className={styles.wrapper}>
       <Breadcrumb title="login" />
-      <LoginForm />
+      <div className={styles["form-wrapper"]}>
+        <h2>{isLogin ? "Login" : "Create an Account"}</h2>
+        <LoginForm isLogin={isLogin} toggleAuthState={toggleAuthState} />
+      </div>
     </section>
   );
 };
--- src/pages/Login/LoginForm/LoginForm.js
+++ src/pages/Login/LoginForm/LoginForm.js
@@ -1,18 +1,96 @@
 import styles from "./LoginForm.module.scss";
+import { useRef } from "react";
 import Button from "../../../components/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { sendAuthRequest } from "../../../features/authSlice";
 
-const LoginForm = () => {
+const LoginForm = ({ isLogin, toggleAuthState }) => {
+  const dispatch = useDispatch();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const { emailInput, passwordInput, isLoading } = useSelector(
+    (state) => state.auth
+  );
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    if (isLogin) {
+      dispatch(
+        sendAuthRequest({
+          email: enteredEmail,
+          password: enteredPassword,
+          requestType: "login",
+        })
+      );
+    } else {
+      dispatch(
+        sendAuthRequest({
+          email: enteredEmail,
+          password: enteredPassword,
+          requestType: "signup",
+        })
+      );
+    }
+  };
+
+  let buttonText;
+  if (isLogin && !isLoading) {
+    buttonText = "Login";
+  }
+  if (isLogin && isLoading) {
+    buttonText = "Logging in...";
+  }
+  if (!isLogin && !isLoading) {
+    buttonText = "Create account";
+  }
+  if (!isLogin && isLoading) {
+    buttonText = "Creating account...";
+  }
+
   return (
-    <form className={styles.wrapper}>
-      <label htmlFor="email" name="email" id="password">
-        Email
-      </label>
-      <input type="text" />
-      <label htmlFor="password" name="password" id="password">
-        Password
-      </label>
-      <input type="password" />
-      <Button fill>Login</Button>
+    <form className={styles.wrapper} onSubmit={submitHandler}>
+      <label htmlFor="email">Email</label>
+      <div className={styles["input-wrapper"]}>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          ref={emailInputRef}
+          required
+        />
+        {emailInput.touched && emailInput.hasError && (
+          <div className={styles["error-msg"]}>{emailInput.error}</div>
+        )}
+      </div>
+      <label htmlFor="password">Password</label>
+      <div className={styles["input-wrapper"]}>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          ref={passwordInputRef}
+          required
+        />
+        {passwordInput.touched && passwordInput.hasError && (
+          <div className={styles["error-msg"]}>{passwordInput.error}</div>
+        )}
+      </div>
+      <Button className={styles.submit} fill disabled={isLoading}>
+        {buttonText}
+      </Button>
+      <div className={styles["btn-container"]}>
+        <p>{isLogin ? "New user?" : "Returning user?"}</p>
+        <Button
+          type="button"
+          className={styles["text-btn"]}
+          onClick={toggleAuthState}
+        >
+          {isLogin ? "Create an account" : "Sign in"}
+        </Button>
+      </div>
     </form>
   );
 };
