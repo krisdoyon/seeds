2816831e11a3d4a682f3b7b84826e481c3aaa51c
Kris Doyon <kristopher.doyon@gmail.com>
2022-12-04 22:11:46 -0500

    Conditionally render new arrivals section once products have been fetched

--- src/pages/Home/NewArrivals/NewArrivals.js
+++ src/pages/Home/NewArrivals/NewArrivals.js
@@ -3,16 +3,17 @@ import { useSelector } from "react-redux";
 import NewArrivalItem from "./NewArrivalItem/NewArrivalItem";
 
 const NewArrivals = () => {
-  const { newProducts } = useSelector((state) => state.products);
+  const { newProducts, isLoading } = useSelector((state) => state.products);
 
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.heading}>New Arrivals</h2>
       <div className={styles["items-wrapper"]}>
         <div className={styles.items}>
-          {newProducts.map((product) => {
-            return <NewArrivalItem key={product.id} {...product} />;
-          })}
+          {!isLoading &&
+            newProducts.map((product) => {
+              return <NewArrivalItem key={product.id} {...product} />;
+            })}
         </div>
       </div>
     </section>
