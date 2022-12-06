f330a0b165bd6bd72879110775e8e1484fb0611c
Kris Doyon <kristopher.doyon@gmail.com>
2022-12-05 22:08:55 -0500

    Move API key to .env file

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,4 +1,3 @@
 export const API_URL = "https://seeds-22950-default-rtdb.firebaseio.com";
-export const API_KEY = "AIzaSyAt1Mw31odpqYNLDT1jLuTkSkVKPofZvz8";
-export const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
-export const LOGIN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+export const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env["REACT_APP_API_KEY"]}`;
+export const LOGIN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env["REACT_APP_API_KEY"]}`;
